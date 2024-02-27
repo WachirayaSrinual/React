8986d5106d4f8834f82007efbd1ee663c380117a
WachirayaSrinual <you@example.com>
2024-02-27 10:37:42 +0700

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import Contact from "./assets/Contact";
+import Counter from "./assets/Counter";
+import Hello from "./assets/Hello";
 
 function App() {
+  const helloData = [
+    {name: 'Wachiraya', surname: 'Srinual'},
+    {name: 'Box', surname: 'Swan'}
+  ];
   return (
-    <div>
-    <h1>Hello</h1>
-    <h2> My First React App.</h2>
+    <div className='App'>
+      <Counter/>
+    {helloData.map((data, index) => (
+      <Hello key={index} name={data.name} surname={data.surname} />
+    ))}
+
+    <Contact email ="s6506021620172@email.kmutnb.ac.ht" phone="061748258x"/>
     </div>
-  )
+  );
 }
 
 export default App
